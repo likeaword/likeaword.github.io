d3410863e9e6c5625d8fb48c2e0b15282a1a0008
likeaword <ben@satorilab.org>
2018-03-16 14:59:46 +0000

    Add files via upload

---
+++ GP_Cultural_Assets/data/Merged_source6_BROADCAT__Events_8.js
@@ -0,0 +1,13 @@
+var json_Merged_source6_BROADCAT__Events_8 = {
+"type": "FeatureCollection",
+"name": "Merged_source6_BROADCAT__Events_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "BROADCAT": "Events", "CATEGORY": "Community Centre", "CAT_OTHER": null, "NAME": "Stanford & Orleton Village Hall", "ADDRESS": null, "WEB": null, "SOURCE": null, "OSM_ID": "3139673250", "CODE": 2012.0, "SURVEY": null, "NOTES": null }, "geometry": { "type": "Point", "coordinates": [ -2.4363145, 52.2903766 ] } },
+{ "type": "Feature", "properties": { "BROADCAT": "Events", "CATEGORY": "Community Centre", "CAT_OTHER": null, "NAME": "Burley Gate Village Hall", "ADDRESS": null, "WEB": null, "SOURCE": null, "OSM_ID": "1677209935", "CODE": 2012.0, "SURVEY": null, "NOTES": null }, "geometry": { "type": "Point", "coordinates": [ -2.5928039, 52.1218917 ] } },
+{ "type": "Feature", "properties": { "BROADCAT": "Events", "CATEGORY": "Community Centre", "CAT_OTHER": null, "NAME": "HOPE Family Centre", "ADDRESS": null, "WEB": null, "SOURCE": null, "OSM_ID": "1677209944", "CODE": 2012.0, "SURVEY": null, "NOTES": null }, "geometry": { "type": "Point", "coordinates": [ -2.5207654, 52.1818904 ] } },
+{ "type": "Feature", "properties": { "BROADCAT": "Events", "CATEGORY": "Community Centre", "CAT_OTHER": null, "NAME": null, "ADDRESS": null, "WEB": null, "SOURCE": null, "OSM_ID": "297464762", "CODE": 2012.0, "SURVEY": null, "NOTES": null }, "geometry": { "type": "Point", "coordinates": [ -3.154905, 52.2401786 ] } },
+{ "type": "Feature", "properties": { "BROADCAT": "Events", "CATEGORY": "Community Centre", "CAT_OTHER": null, "NAME": "Llanigon Village Hall", "ADDRESS": null, "WEB": null, "SOURCE": null, "OSM_ID": "3428897320", "CODE": 2012.0, "SURVEY": null, "NOTES": null }, "geometry": { "type": "Point", "coordinates": [ -3.1517884, 52.0520519 ] } },
+{ "type": "Feature", "properties": { "BROADCAT": "Events", "CATEGORY": "Community Centre", "CAT_OTHER": null, "NAME": null, "ADDRESS": null, "WEB": null, "SOURCE": null, "OSM_ID": "5220403932", "CODE": 2012.0, "SURVEY": null, "NOTES": null }, "geometry": { "type": "Point", "coordinates": [ -2.7140914, 51.8126442 ] } }
+]
+}
